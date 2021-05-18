1d123a2652480d7a42e2bfb5f9b3c052f03f95b1
matiasmaizares <72669598+matiasmaizares@users.noreply.github.com>
2021-05-18 16:13:35 -0300

    make it better

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postsRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/posts', postsRoutes);
+
+app.get('/', (req, res) => {
+  res.send('hello memories api');
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server runnig on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
